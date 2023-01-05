3c245187363ab24c6ad0d2b4d2907d7f788bdac8
Sanath <sanath@entrusttitle.com>
2023-01-05 12:42:55 +0530

    Setup the Dependency Injection with Dagger Hilt

--- app/src/main/java/com/sanath/cryptocurrencyapp/common/Constants.kt
+++ app/src/main/java/com/sanath/cryptocurrencyapp/common/Constants.kt
@@ -1,4 +1,7 @@
 package com.sanath.cryptocurrencyapp.common
 
 object Constants {
+
+    const val BASE_URL = "https://api.coinpaprika.com/"
+
 }
---
+++ app/src/main/java/com/sanath/cryptocurrencyapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.sanath.cryptocurrencyapp.di
+
+import com.sanath.cryptocurrencyapp.common.Constants.BASE_URL
+import com.sanath.cryptocurrencyapp.data.remote.CoinPaprikaApi
+import com.sanath.cryptocurrencyapp.data.repository.CoinRepositoryImpl
+import com.sanath.cryptocurrencyapp.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+
+}
